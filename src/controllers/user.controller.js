321a030d92850d66c885cd178005409f6e3d5750
SarthakGajera <sarthakgajera91@gmail.com>
2024-12-26 18:07:55 +0530

    user logout and auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -95,6 +95,9 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
+
+
+
 const loginUser = asyncHandler(async (req, res) => {
   //req body->data
   //username or email
@@ -151,4 +154,28 @@ const loginUser = asyncHandler(async (req, res) => {
 
 });
 
-export { registerUser, loginUser };
+
+const logoutUser=asyncHandler(async(req,res)=>{
+ await User.findByIdAndUpdate(
+  req.user._id,
+  {
+    $set:{
+      refreshToken:undefined
+    }
+  },
+  {
+    new:true
+  }
+ )
+ const options={
+  httpOnly:true,
+  secure:true
+}
+return res
+.status(200)
+.clearCokkie("accessToken",options)
+.clearCokkie("refreshToken",options)
+.json(new ApiResponse(200,{},"User logout successfully"))
+})
+
+export { registerUser, loginUser , logoutUser };
