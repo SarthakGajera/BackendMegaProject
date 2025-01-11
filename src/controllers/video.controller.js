695af74b176ca0eb4dea6cb34d3e53462e3c5287
SarthakGajera <sarthakgajera91@gmail.com>
2025-01-11 22:36:07 +0530

    add video controller and new files created

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,22 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(400, "no such video exist");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Successfully got the video"));
+});
+
